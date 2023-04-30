e410f56de718f46405e0a25d2d3ee40eee0d5dc1
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-04-30 16:38:12 +0700

    Init Nara Kernel

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Katana Kernel 4.19 for Xiaomi SM6115 platform
+kernel.string=Nara Kernel 4.19 for Xiaomi SM6115 platform
 do.devicecheck=1
 do.modules=0
 do.systemless=1
