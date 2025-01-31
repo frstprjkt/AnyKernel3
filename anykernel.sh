2d09937e19cdf25bf02f154bc698bf3d47001145
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2025-02-01 06:39:25 +0700

    valeryn go brr....

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=frstprjkt@GitHub random kernel
+kernel.string=Valeryn kernel by frstprjkt@GitHub
 do.devicecheck=0
 do.modules=0
 do.systemless=0
